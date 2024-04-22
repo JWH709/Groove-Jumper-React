86f0a4845e3cc316b01c67abc03f0098dcc84bec
John Heneghan <jwheneghan@hotmail.com>
2024-04-22 09:51:11 -0230

    Initial commit

---
+++ src/assets/AlbumPools.js
@@ -0,0 +1,241 @@
+const albumPools = [
+  [
+    [
+      {
+        submittedBy: "Jack",
+        album: "Sountracks for the Blind",
+        url: "https://api.discogs.com/masters/8895",
+        albumCover:
+          "https://i.discogs.com/1kVMCE0M2vRuBUBxpcy5GTHY2cqECn2ur8VGk_jo5JY/rs:fit/g:sm/q:90/h:302/w:340/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMyOTc5/Ny0xMTAzMTEzMDI3/LmpwZw.jpeg",
+      },
+      {
+        submittedBy: "Jack",
+        album: "Songs for the Deaf",
+        url: "https://api.discogs.com/masters/3239",
+        albumCover:
+          "https://i.discogs.com/RrY6_HSWVQPSyV_Y8Nylk7l8d8Fm-44FHIsZIhdGmgw/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM4NDcx/My0xNTIwMjA4MTk1/LTUwMjEuanBlZw.jpeg",
+      },
+    ],
+    [
+      {
+        submittedBy: "Jack",
+        album: "Sea of Worry",
+        url: "https://api.discogs.com/masters/1629425",
+        albumCover:
+          "https://i.discogs.com/Jn_xrC0FW01j6LHZlD1KLFPUs5hvfM9tXD0rIOmH_dU/rs:fit/g:sm/q:90/h:532/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE0MzQ5/OTY1LTE1NzI4NDI5/NzMtNjExNi5qcGVn.jpeg",
+      },
+      {
+        submittedBy: "Jack",
+        album: "Sea of no Cares",
+        url: "https://api.discogs.com/masters/662989",
+        albumCover:
+          "https://i.discogs.com/wZtPZOu41RZN3mvtgU1Qd5xF2qDDH2w5ikzetDq5dZg/rs:fit/g:sm/q:90/h:572/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTUyOTE2/NjYtMTY4NDIyNTE4/MS04OTIxLmpwZWc.jpeg",
+      },
+    ],
+  ],
+  [
+    [
+      {
+        submittedBy: "Riley",
+        album: "Good Kid, Mad City",
+        url: "https://api.discogs.com/masters/484590",
+        albumCover:
+          "https://i.discogs.com/NlmkKrMlFUvKaaW5GUSKFRrMB1snZbF9Zx6_fMUsBtY/rs:fit/g:sm/q:90/h:588/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM5NzU5/NTMtMTU3MDQwNDUz/Ni01NDY1LmpwZWc.jpeg",
+      },
+      {
+        submittedBy: "Riley",
+        album: "Kid A",
+        url: "https://api.discogs.com/masters/21501",
+        albumCover:
+          "https://i.discogs.com/_zg0Us0N_anqCz9hJvFGeRK2v69eDDCxQnHkd7RcgTk/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc0NzQz/LTE1NDg5MDI2NDMt/Mzg2OS5qcGVn.jpeg",
+      },
+    ],
+    [
+      {
+        submittedBy: "Riley",
+        album: "Big Fish Theory",
+        url: "https://api.discogs.com/masters/1200128",
+        albumCover:
+          "https://i.discogs.com/8jHRj4WwFg-SGF8VYq6FfzGa2pGWwflBA62gZShFrrM/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwNDU0/OTY4LTE1MDAwMzIx/NDMtNjQwNS5qcGVn.jpeg",
+      },
+      {
+        submittedBy: "Riley",
+        album: "Trout Mask Replica",
+        url: "https://api.discogs.com/masters/463963",
+        albumCover:
+          "https://i.discogs.com/CxeVQmJ2DM006onzI_oDbu0s0I7NCH61h8cRngPrwpE/rs:fit/g:sm/q:90/h:600/w:576/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI2NjU2/OTktMTYxMzk1MDg0/OC02NTQ4LmpwZWc.jpeg",
+      },
+    ],
+  ],
+  [
+    [
+      {
+        submittedBy: "Braden",
+        album: "California",
+        url: "https://api.discogs.com/masters/1021870",
+        albumCover:
+          "https://i.discogs.com/vASPy_CtY4GSdD0Sth0InkqFbCg9-hzgceDRYOddL84/rs:fit/g:sm/q:90/h:542/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTg3MTMy/MzAtMTQ2NzE1MzU4/MC0zMTYzLmpwZWc.jpeg",
+      },
+      {
+        submittedBy: "Braden",
+        album: "Californiacation",
+        url: "https://api.discogs.com/masters/42546",
+        albumCover:
+          "https://i.discogs.com/CyHLgusU6vKUZ6AZbiWlLpgtKm2mT05k0NdV5LQvMjM/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNjY4/MDQtMTQ2MTI5MDIx/Ni03MDEwLmpwZWc.jpeg",
+      },
+    ],
+    [
+      {
+        submittedBy: "Ben",
+        album: "Ugly but Honest",
+        url: "https://api.discogs.com/masters/282690",
+        albumCover:
+          "https://i.discogs.com/mdIdk0myc4BpSwO4wRiLI5vTEJbvQRaGH7GMCWrWwio/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTYzMDgx/My0xNjA0NzMxMTU3/LTU5NDkuanBlZw.jpeg",
+      },
+      {
+        submittedBy: "Ben",
+        album: "A Beautiful Lie",
+        url: "https://api.discogs.com/masters/95121",
+        albumCover:
+          "https://i.discogs.com/W032BPhXc4TF_63t04c81lsrnydZsZEpsNzwiwIpkhg/rs:fit/g:sm/q:90/h:523/w:562/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2NzQx/NTctMTQ0MDAzNjU4/MS03Nzk0LmpwZWc.jpeg",
+      },
+    ],
+  ],
+  [
+    [
+      {
+        submittedBy: "Liam",
+        album: "Marquee Moon",
+        url: "https://api.discogs.com/masters/6202",
+        albumCover:
+          "https://i.discogs.com/CRbZRWmZLpLIo0wKdsLJArcMd1NvHE8Qyyd1x22roSk/rs:fit/g:sm/q:90/h:599/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTg3NjUz/MDQtMTQ2ODI5Mzk3/NS0zMTc0LmpwZWc.jpeg",
+      },
+      {
+        submittedBy: "Liam",
+        album: "...And Don't the Kids Just Love it",
+        url: "https://api.discogs.com/masters/42308",
+        albumCover:
+          "https://i.discogs.com/xR7qjW5ktNqjDwDCTjCC_30bRl7zgjZBeN_QspAmnMo/rs:fit/g:sm/q:90/h:600/w:594/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTgxNzMx/OC0xMzM0NDIzMTc4/LmpwZWc.jpeg",
+      },
+    ],
+    [
+      {
+        submittedBy: "Liam",
+        album: "Pink Flag",
+        url: "https://api.discogs.com/masters/50645",
+        albumCover:
+          "https://i.discogs.com/cknT5IAagyDTJrkxbuC42GhV07djI_2Ca8kooTUF134/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIyMzM1/NC0xMzgxOTQ3MDg1/LTc4MTMuanBlZw.jpeg",
+      },
+      {
+        submittedBy: "Liam",
+        album: "Black Flag",
+        url: "https://api.discogs.com/masters/1404382",
+        albumCover:
+          "https://i.discogs.com/amuFgmvf17rrbrbLOhFM7xT8HYfcwPj6HHnsx753geE/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQ2OTEy/MzQtMTQyODE2NTE2/OC01NjM3LmpwZWc.jpeg",
+      },
+    ],
+  ],
+  [
+    [
+      {
+        submittedBy: "Jordan",
+        album: "Finally Rich",
+        url: "https://api.discogs.com/masters/701431",
+        albumCover:
+          "https://i.discogs.com/G0McalaLCoaz5ggjW5lLAZoroiNuK9wewZBV5m0gXJA/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIxMzQ5/ODI1LTE2Mzk1MDMy/OTAtOTA4MS5qcGVn.jpeg",
+      },
+      {
+        submittedBy: "Jordan",
+        album: "Lifestylez Ov Da Poor & Dangerous",
+        url: "https://api.discogs.com/masters/53775",
+        albumCover: "",
+      },
+    ],
+    [
+      {
+        submittedBy: "Jordan",
+        album: "Dogs On Acid",
+        url: "https://api.discogs.com/masters/901897",
+        albumCover:
+          "https://i.discogs.com/lmeBySeMmv4GdnUsxzsDupMu3bW-DosGYRhfhEm-bGc/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc0MzQ1/NzYtMTQ0MTQ1NTA5/Ni0zNDc4LmpwZWc.jpeg",
+      },
+      {
+        submittedBy: "Jordan",
+        album: "Cats Under The Stars",
+        url: "https://api.discogs.com/masters/344520",
+        albumCover:
+          "https://i.discogs.com/ZHww7n31Sxt1_aQd1eMWTZ1vd5zVuH6n3IoZ7kb0hvk/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTU0NzMy/MDMtMTM5NjU0MzQ0/Ny05NzgwLmpwZWc.jpeg",
+      },
+    ],
+  ],
+  [
+    [
+      {
+        submittedBy: "Riley",
+        album: "Scream, Aim, Fire",
+        url: "https://api.discogs.com/masters/232474",
+        albumCover:
+          "https://i.discogs.com/rA_DqS8BQ_-4kUTRvDIc0tMi4q1BGB4O7etjNGI7SDM/rs:fit/g:sm/q:90/h:366/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIyNzIw/MTktMTI4NDU1MzM0/MS5qcGVn.jpeg",
+      },
+      {
+        submittedBy: "Riley",
+        album: "Loveless",
+        url: "https://api.discogs.com/masters/5948",
+        albumCover:
+          "https://i.discogs.com/BX3VaGJBENv-zgHEdez23DDFIVQlaSWGgdr_8hG6dWA/rs:fit/g:sm/q:90/h:280/w:280/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNzYy/Mi0xMjUwMjA4Njc5/LmpwZWc.jpeg",
+      },
+    ],
+    [
+      {
+        submittedBy: "Liam",
+        album: "Whole Lotta Red",
+        url: "https://api.discogs.com/masters/2057692",
+        albumCover:
+          "https://i.discogs.com/R__ClQV_qnaV0ITzx0pwkzZfcdbC5F4E5MyTUNaf7TU/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2NjE4/MzE3LTE2MTAyODEy/NzUtODcyMy5qcGVn.jpeg",
+      },
+      {
+        submittedBy: "Liam",
+        album: "Kind of Blue",
+        url: "https://api.discogs.com/masters/5460",
+        albumCover:
+          "https://i.discogs.com/wjjMk1xCKdaPbsUbaXWzHBNi3K-fmt3CYkQGqt0c_Z8/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTYyNzYx/ODMtMTY3NzAwMzM5/Ni0yMDQ1LmpwZWc.jpeg",
+      },
+    ],
+  ],
+  [
+    [
+      {
+        submittedBy: "James",
+        album: "Triangle",
+        url: "https://api.discogs.com/masters/150447",
+        albumCover:
+          "https://i.discogs.com/HhxmJIAjinJzKVa7fA95Ahfwd9nCfoDUb4_JTqY9RHI/rs:fit/g:sm/q:90/h:587/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE4NDI1/ODMtMTI0NzE5NDMz/OS5qcGVn.jpeg",
+      },
+      {
+        submittedBy: "James",
+        album: "Circles",
+        url: "https://api.discogs.com/masters/1669934",
+        albumCover:
+          "https://i.discogs.com/-Cq1NLkCM-inLcEhOEQTBdY1Sgvg3QkC2-kU_STKDFA/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIxMDI4/MTQ3LTE2MzcyNDMw/MjEtNTkxMC5qcGVn.jpeg",
+      },
+    ],
+    [
+      {
+        submittedBy: "James",
+        album: "Frances the Mute",
+        url: "https://api.discogs.com/masters/9161",
+        albumCover:
+          "https://i.discogs.com/T_GoAoUfYEmMeCnWT8QriZQV5faHsz41sKdzZIlHVQk/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQ2MzMz/Ni0xNTUyNTI2NDQ0/LTc0NzEuanBlZw.jpeg",
+      },
+      {
+        submittedBy: "James",
+        album: "Speaking in Toungues",
+        url: "https://api.discogs.com/masters/39336",
+        albumCover:
+          "https://i.discogs.com/w3G0LxzwepwIgx7NkozV23OhlQ3j0XZ3WsvHPH6J1DQ/rs:fit/g:sm/q:90/h:595/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE3MDcx/OS0xNDM3NDU0NzA4/LTQ4OTQuanBlZw.jpeg",
+      },
+    ],
+  ],
+];
+
+export default albumPools;
---
+++ src/assets/ApiInfo.js
@@ -0,0 +1,3 @@
+const token = "token=QclYwHOWDnQzeGlmzPcvVVjXcjjxQTckcCIoxQOT";
+
+export default token;
