a0ee539b6be4a7def552db9349160f71f400d230
John Heneghan <jwheneghan@hotmail.com>
2024-04-22 11:37:42 -0230

    begin API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,63 @@
+/* eslint-disable react/prop-types */
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import albumPools from "./assets/AlbumPools";
 
 const App = () => {
+  const startingAlbumReference = albumPools[0][0][1];
+
+  const [startingAlbum, setStartingAlbum] = React.useState();
+
+  React.useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`${startingAlbumReference.url}`);
+      const post = await response.json();
+      setStartingAlbum(post);
+    };
+
+    fetchPost();
+  });
+
+  console.log(startingAlbum); //for checking what properties I'm using
+
   return (
     <>
       <InfoTab />
-      <GameTab />
+      <GameTab
+        title={startingAlbum.title}
+        cover={startingAlbumReference.albumCover}
+        albumArtist={startingAlbum.artists[0].name}
+      />
     </>
   );
 };
 
 const InfoTab = () => {};
 
-const GameTab = () => {
+const GameTab = ({ title, cover, albumArtist }) => {
   return (
     <>
-      <GameCard />
+      <GameCard
+        cardTitle={title}
+        cardCover={cover}
+        cardAlbumArtist={albumArtist}
+      />
       <GameSearch />
       <PreviousConnections />
     </>
   );
 };
 
-const GameCard = () => {
+const GameCard = ({ cardTitle, cardCover, cardAlbumArtist }) => {
   return (
     <>
       <Card style={{ width: "18rem" }}>
         <Card.Body>
-          <Card.Title>Album Title</Card.Title>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-          <Card.Text>By: Album Artist</Card.Text>
+          <Card.Title>{cardTitle}</Card.Title>
+          <Card.Img variant="top" src={cardCover} />
+          <Card.Text>By: {cardAlbumArtist}</Card.Text>
         </Card.Body>
         <ListGroup className="list-group-flush">
           <ListGroup.Item>Streak</ListGroup.Item>
